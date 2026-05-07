94ebe9e640b0cd8739bb591eee0803d16a2c78df
Srinivasa Gudi <239487102+srinivasagudi0@users.noreply.github.com>
2026-05-07 08:16:55 -0500

    Change sidebar header to markdown format
    Will stop makein it look better now and start working

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 # I will try to make level 4 project and this should help all the beginners and also me.
 
-st.sidebar.header("GitPilot")
+st.sidebar.markdown("# **:blue[GitPilot]**")
 feature = st.sidebar.selectbox("Choose a feature", ["Help"])
 
 # first before going any deeper I will add "help" as a feature and then I will add more features later on. It wil also help me to shape the project in a better way.

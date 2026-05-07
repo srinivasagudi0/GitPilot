a1b771fe709b9af1e5ac33eadb288b0bc258afcc
Srinivasa Gudi <239487102+srinivasagudi0@users.noreply.github.com>
2026-05-07 08:14:36 -0500

    Refactor layout and update sidebar header
    Removed unused columns and updated sidebar header.

--- app.py
+++ app.py
@@ -1,15 +1,9 @@
 import streamlit as st
 
-top, bottom = st.columns(2)
-with top:
-    left, right = st.columns(2)
-    with left:
-        st.title("GitPilot")
-    with right:
-        st.write("Thinking what to write here")
+
 # I will try to make level 4 project and this should help all the beginners and also me.
 
-st.sidebar.header("Select a feature")
+st.sidebar.header("GitPilot")
 feature = st.sidebar.selectbox("Choose a feature", ["Help"])
 
 # first before going any deeper I will add "help" as a feature and then I will add more features later on. It wil also help me to shape the project in a better way.
